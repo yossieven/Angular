8aa0023c93135e78fb0c33e8c798c899d27c5abc
Yossi Even <yossi.even@gmail.com>
2019-01-17 00:49:56 +0200

    working observable

--- shop/src/app/products.service.ts
+++ shop/src/app/products.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
+import { map, tap } from 'rxjs/operators';
 import { DecimalPipe } from '@angular/common';
 import * as _ from 'lodash';
 
@@ -13,30 +14,36 @@ export interface Product {
   image: string
 }
 
+export interface Response {
+  success: boolean,
+  data: Product[]
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  public products: Product[];
+  public products = new BehaviorSubject<Product[]>([]);
 
   constructor(private http: HttpClient) {
 
   }
 
-  getProducts(specific: string) {
+  async getProducts(specific: string) {
     const basicURL = 'http://localhost:3000/api/products/';
     const params = specific || '';
     const finalURL = basicURL + params;
     console.log("URL", finalURL);
-    return this.http.get<Product[]>("http://localhost:3000/products/").do(console.log);
-    console.log("products", products);
-    // this.http.get(finalURL).pipe(
-    //   map(
-    //     (response: Product[]) => {
-    //       console.log("response", response);
-    //       this.products.next(response);
-    //       return response;
-    //     })
-    // )
+    const response = await this.http.get(finalURL);
+    console.log(response);
+
+    // console.log("products", this.products);
+    this.http.get(finalURL).pipe(
+      map(
+        (response: Response) => {
+          return response.data;
+
+        })).subscribe(response => this.products.next(response))
   }
 }
